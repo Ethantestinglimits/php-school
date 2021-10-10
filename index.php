86cbb4a044d004b95d72c203298b97a92a0a4800
ethanito <bcrethan@gmail.com>
2021-10-10 22:07:42 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Projet Film</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, user-scalable=no,  -scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <
+</head>
+
+
+<body>
+
+<div>
+    <ul>
+        <?php
+
+        foreach(movie != null) {
+
+            foreach () {
+
+
+                echo
+        }
+            foreach () {
+
+            }
+
+            echo '<li></li>';
+        }
+
+        ?>
+    </ul>
+</div>
+
+</body>
+</html>
+
